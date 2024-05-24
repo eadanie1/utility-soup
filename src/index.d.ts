7653115cea4d82cab31755efe889f2ccc7790091
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-05-24 20:55:20 +0200

    Remove src directory from repository

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface UtilitySoup {
-  increment: (count: number) => number;
-  decrement: (count: number) => number;
-  upperCaseify: (text: string) => string;
-  dateToText: () => string;
-  sortArray: (array: number[]) => number[];
-}
